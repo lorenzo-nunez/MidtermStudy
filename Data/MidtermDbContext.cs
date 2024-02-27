e1354b7fc2d5241d66d2ca97e8a3cfd47b792b32
loren <loren@LORENZO>
2024-02-26 21:22:41 -0500

    Add project files.

---
+++ Data/MidtermDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MidtermStudy.Models;
+
+namespace MidtermStudy.Data
+{
+    public class MidtermDbContext : DbContext
+    {
+        public MidtermDbContext(DbContextOptions<MidtermDbContext> options)
+            :base(options)
+        {
+            
+        }
+
+        //Step 3 (Creating Model) STUDY GUIDE!!!!!
+        public DbSet<Category> Categories { get; set; }
+
+        //Step 4 (Creating model)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+
+                new Category { 
+                    Id = 1, 
+                    Name = "Travel", 
+                    Description = "Example stuff" },
+                new Category { 
+                    Id = 2, 
+                    Name = "Fiction", 
+                    Description = "Example stuff" },
+                new Category { 
+                    Id = 3, 
+                    Name = "Non-Fiction", 
+                    Description = "Example stuff" }
+                );
+        }
+    }
+}
---
+++ Migrations/20240227020319_creatingCategories.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MidtermStudy.Migrations
+{
+    /// <inheritdoc />
+    public partial class creatingCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Example stuff", "Travel" },
+                    { 2, "Example stuff", "Fiction" },
+                    { 3, "Example stuff", "Non-Fiction" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
